1d920de73aa678be4b42bf40fee438b6c9b2c6db
domakedev <sarcansino@gmail.com>
2025-09-19 01:38:48 -0500

    feat: simplificar la gestión de estado en los botones de copiar y pegar

--- src/components/CopyPasteButtons.tsx
+++ src/components/CopyPasteButtons.tsx
@@ -20,7 +20,7 @@ const CopyPasteButtons: React.FC<CopyPasteButtonsProps> = ({ activeTab }) => {
             toast.success("Tarea copiada limpiada.");
           }}
           disabled={!copiedTask}
-          className="flex items-center space-x-1 sm:space-x-2 bg-red-600 text-white text-xs sm:text-sm font-medium py-1 px-2 sm:py-2 sm:px-4 rounded-md sm:rounded-lg shadow-sm sm:shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-75 transition-transform transform hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed"
+          className="flex items-center space-x-1 sm:space-x-2 bg-red-600 text-white text-xs sm:text-sm font-medium py-1 px-2 sm:py-2 sm:px-4 rounded-md sm:rounded-lg shadow-sm sm:shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-75 transition-transform transform hover:scale-105 disabled:opacity-50"
         >
           <Icon name="trash2" className="h-3 w-3 sm:h-4 sm:w-4" />
           <span>Limpiar Copia</span>
@@ -28,9 +28,8 @@ const CopyPasteButtons: React.FC<CopyPasteButtonsProps> = ({ activeTab }) => {
       )}
       <button
         onClick={() => handlePasteTask(activeTab)}
-        className={`flex items-center space-x-1 sm:space-x-2 bg-slate-600 text-white text-xs sm:text-sm font-medium py-1 px-2 sm:py-2 sm:px-4 rounded-md sm:rounded-lg shadow-sm sm:shadow-md hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-opacity-75 transition-transform transform hover:scale-105 ${
-          !copiedTask ? "cursor-not-allowed opacity-50" : ""
-        }`}
+        disabled={!copiedTask}
+        className={`flex items-center space-x-1 sm:space-x-2 bg-slate-600 text-white text-xs sm:text-sm font-medium py-1 px-2 sm:py-2 sm:px-4 rounded-md sm:rounded-lg shadow-sm sm:shadow-md hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-opacity-75 transition-transform transform hover:scale-105 disabled:opacity-50`}
       >
         <Icon name="clipboard" className="h-3 w-3 sm:h-4 sm:w-4" />
         <span>Pegar</span>
